175910d4f9dd03990166bac3e80d6f8bbcce67ed
HalnyGit <haliniakm@gmail.com>
2017-03-25 20:04:31 +0100

    condition 0 changed

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 // Zadanie 8
 
 var a = 5,
-	b = 7,
+	b = 70,
 	value = (a * a) + (2 * a * b) - (b * b);
 
 console.log(value);
 
 if (value > 0) {
 	console.log('Wynik dodatni');
-} else if (value = 0) {
+} else if (value == 0) {
 	consle.log('Wynik zerowy');
 } else {
 	console.log('Wynik ujemny');
