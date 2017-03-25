f2db593c1e25beaaf4b01e6dbc7bbc4e9b847fab
HalnyGit <haliniakm@gmail.com>
2017-03-25 18:39:15 +0100

    funckje

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
 // Zadanie 8
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var a = 5,
+	h = 10,
+	triangleArea = 0,
+	triangle1Area = 0,
+	triangle2Area = 0,
+	triangle3Area = 0;
+
+function getTriangleArea(a,h) {
+	return a * h / 2
+};
+
+if ((a<=0) || (h<=0)) {
+	console.log('Nieprawidłowe dane');
+} else {
+	triangleArea=getTriangleArea(a,h);
+	console.log(triangleArea);
+};
+
+triangle1Area=getTriangleArea(2,10);
+triangle2Area=getTriangleArea(20,2);
+triangle3Area=getTriangleArea(7,5);
+
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
+
+
+
+
+
 
