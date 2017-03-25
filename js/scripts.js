a7935fe87e2aa64d6cf52b5509c7ce6986b65152
HalnyGit <haliniakm@gmail.com>
2017-03-25 17:45:45 +0100

    instrukcje warunkowe

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 // Zadanie 8
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+
+var a = 5,
+	b = 7,
+	value = (a * a) + (2 * a * b) - (b * b);
+
+console.log(value);
+
+if (value > 0) {
+	console.log('Wynik dodatni');
+} else if (value = 0) {
+	consle.log('Wynik zerowy');
+} else {
+	console.log('Wynik ujemny');
+}
 
