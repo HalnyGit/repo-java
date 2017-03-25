89f35736398e01249cca245c3ab758e53edd6464
HalnyGit <haliniakm@gmail.com>
2017-03-25 21:31:52 +0100

    Papugi i słońe

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 // Zadanie 8
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+var animalUpperCased = animal.toUpperCase();
+var textReplaced = text.replace('Papugi', animal);
+var half = (textReplaced.length) / 2;
+var halfTextReplaced = textReplaced.substr(0, half);
+console.log(halfTextReplaced);
 
