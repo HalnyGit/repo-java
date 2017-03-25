a058eeba2caa032076965cc295252fe2005e6fe0
HalnyGit <haliniakm@gmail.com>
2017-03-25 19:48:06 +0100

    tables

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 // Zadanie 8
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = [];
+var newName = 'Asia';
 
+var allNames = allNames.concat(femaleNames,maleNames);
+
+if (allNames.indexOf(newName) === -1) {
+	allNames.push(newName);
+};
+
+console.log(allNames);
